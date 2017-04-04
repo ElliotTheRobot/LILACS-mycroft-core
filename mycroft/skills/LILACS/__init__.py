5e08e6c94d0722aaa4c6512ec12833c9d8a9b32b
Heinz Schmidt <heinz@heinzschmidt.co.uk>
2017-04-04 15:45:03 +0100

    Added ConceptStorage class

--- mycroft/skills/LILACS/__init__.py
+++ mycroft/skills/LILACS/__init__.py
@@ -154,8 +154,6 @@ class ConceptStorage():
 
     def __init__(self, storagetype="json", database="lilacstorage.db"):
         self._dataStorageType = storagetype
-        #self._dataStorageUser = username
-        #self._dataStoragePass = password
         self._dataStorageDB = database
         self.datastore_connect()
 
@@ -169,7 +167,7 @@ class ConceptStorage():
                 print(("Database connection failed" + str(sqlerr)))
                 self._dataConnStatus = 0
             """
-        else:
+        elif(self._dataStorageType == "json")::
             with open(dirname(__file__) + "/.db/" + self._dataStorageDB)\
              as datastore:
                 self._dataJSON = json.load(datastore)
@@ -201,4 +199,12 @@ class ConceptStorage():
                 print(('Concept: ' + conceptname))
                 print(('Parent (' + str(generation + 1) + ') : ' +
                     str(p["parents"][0][str(generation)])))
+
+    def getNodeLastUpdate(self, conceptname="cow", generation=0):
+        print("\r")
+        if(self._dataConnStatus == 1):
+            for p in self._dataJSON[conceptname]:
+                print(('Concept: ' + conceptname))
+                print(('Last Update: ' +
+                    str(p["last_update"])))
 

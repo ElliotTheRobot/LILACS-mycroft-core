ce940f19ee27e8268acd4e454ea52195506a69e6
Heinz Schmidt <heinz.schmidt@sfjuk.com>
2017-04-04 22:02:47 +0100

    Fixed defs for single or multiple node retrieval

--- mycroft/skills/LILACS/__init__.py
+++ mycroft/skills/LILACS/__init__.py
@@ -146,66 +146,101 @@ class ConceptCreator():
 
 class ConceptStorage():
 
-	_dataStorageType = ""
-	_dataStorageUser = ""
-	_dataStoragePass = ""
-	_dataStorageDB = ""
-	_dataConnection = None
-	_dataConnStatus = 0
-	_dataJSON = None
+    _dataStorageType = ""
+    _dataStorageUser = ""
+    _dataStoragePass = ""
+    _dataStorageDB = ""
+    _dataConnection = None
+    _dataConnStatus = 0
+    _dataJSON = None
 
-	def __init__(self, storagetype="json", database="lilacstorage.db"):
-		self._dataStorageType = storagetype
-		self._dataStorageDB = database
-		self.datastore_connect()
+    def __init__(self, storagetype="json", database="lilacstorage.db"):
+        self._dataStorageType = storagetype
+        self._dataStorageDB = database
+        self.datastore_connect()
 
-	def datastore_connect(self):
-		if(self._dataStorageType == "sqllite3"):
-			"""try:
-				self._dataConnection = sqllite3.connect(self._dataStorageDB)
-				self._dataConnStatus = 1
-			except Exception as sqlerr:
-				# log something
-				print(("Database connection failed" + str(sqlerr)))
-				self._dataConnStatus = 0
-			"""
-		elif(self._dataStorageType == "json"):
-			with open(dirname(__file__) + "/.db/" + self._dataStorageDB)\
-			 as datastore:
-				self._dataJSON = json.load(datastore)
+    def datastore_connect(self):
+        if(self._dataStorageType == "sqllite3"):
+            """try:
+                self._dataConnection = sqllite3.connect(self._dataStorageDB)
+                self._dataConnStatus = 1
+            except Exception as sqlerr:
+                # log something
+                print(("Database connection failed" + str(sqlerr)))
+                self._dataConnStatus = 0
+            """
+        elif(self._dataStorageType == "json"):
+            with open("db/" + self._dataStorageDB)\
+             as datastore:
+                self._dataJSON = json.load(datastore)
 
-			if(self._dataJSON):
-				self._dataConnStatus = 1
-			else:
-				self._dataConnStatus = 0
+            if(self._dataJSON):
+                self._dataConnStatus = 1
+            else:
+                self._dataConnStatus = 0
 
-	def getNodeDataDictionary(self, conceptname="cow", conceptid=0):
-		print("\n")
-		if(self._dataConnStatus == 1):
-			for p in self._dataJSON[conceptname]:
-				print(('Concept: ' + conceptname))
-				print(('Dictionary: ' + p["data_dict"]))
+    def getNodeDataDictionary(self, conceptname="cow"):
+        returnVal = {}
+        if(self._dataConnStatus == 1):
+            for p in self._dataJSON[conceptname]:
+                returnVal["data_dict"] = str(p["data_dict"])
+        return returnVal
 
-	def getNodeParent(self, conceptname="cow", generation=0):
-		print("\r")
-		if(self._dataConnStatus == 1):
-			for p in self._dataJSON[conceptname]:
-				print(('Concept: ' + conceptname))
-				print(('Parent (' + str(generation + 1) + ') : ' +
-					str(p["parents"][0][str(generation)])))
+    def getNodeParent(self, conceptname="cow", generation=None):
+        returnVal = {}
+        if(self._dataConnStatus == 1):
+            for node in self._dataJSON[conceptname]:
+                if(generation is None):
+                    for parent in node["parents"]:
+                        returnVal = parent
+                elif(generation <= len(node["parents"])):
+                    for parent in node["parents"]:
+                        if parent[str(generation)]:
+                            returnVal = parent[str(generation)]
+        return returnVal
 
-	def getNodeChildren(self, conceptname="cow", generation=0):
-		print("\r")
-		if(self._dataConnStatus == 1):
-			for p in self._dataJSON[conceptname]:
-				print(('Concept: ' + conceptname))
-				print(('Parent (' + str(generation + 1) + ') : ' +
-					str(p["parents"][0][str(generation)])))
+    def getNodeChildren(self, conceptname="cow", generation=None):
+        returnVal = {}
+        if(self._dataConnStatus == 1):
+            for node in self._dataJSON[conceptname]:
+                if(generation is None):
+                    for child in node["children"]:
+                        returnVal = child
+                elif(generation <= len(node["children"])):
+                    for child in node["children"]:
+                        if child[str(generation)]:
+                            returnVal = child[str(generation)]
+        return returnVal
 
-	def getNodeLastUpdate(self, conceptname="cow", generation=0):
-		print("\r")
-		if(self._dataConnStatus == 1):
-			for p in self._dataJSON[conceptname]:
-				print(('Concept: ' + conceptname))
-				print(('Last Update: ' +
-					str(p["last_update"])))
+    def getNodeSynonymn(self, conceptname="cow", generation=None):
+        returnVal = {}
+        if(self._dataConnStatus == 1):
+            for node in self._dataJSON[conceptname]:
+                if(generation is None):
+                    for synonymn in node["synonymns"]:
+                        returnVal = synonymn
+                elif(generation <= len(node["synonymns"])):
+                    for synonymn in node["synonymns"]:
+                        if synonymn[str(generation)]:
+                            returnVal = synonymn[str(generation)]
+            return returnVal
+
+    def getNodeAntonymn(self, conceptname="cow", generation=None):
+        returnVal = {}
+        if(self._dataConnStatus == 1):
+            for node in self._dataJSON[conceptname]:
+                if(generation is None):
+                    for synonymn in node["antonymns"]:
+                        returnVal = synonymn
+                elif(generation <= len(node["antonymns"])):
+                    for synonymn in node["antonymns"]:
+                        if synonymn[str(generation)]:
+                            returnVal = synonymn[str(generation)]
+            return returnVal
+
+    def getNodeLastUpdate(self, conceptname="cow"):
+        returnVal = {}
+        if(self._dataConnStatus == 1):
+            for p in self._dataJSON[conceptname]:
+                returnVal["last_update"] = str(p["last_update"])
+        return returnVal

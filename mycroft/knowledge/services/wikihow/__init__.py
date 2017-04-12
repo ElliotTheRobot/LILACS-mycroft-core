8b34270438e72e5981530af62ac0cc6f447c7216
jarbasai <jarbasai>
2017-04-12 19:51:21 +0100

    wikihow backend

--- mycroft/knowledge/services/wikihow/__init__.py
+++ mycroft/knowledge/services/wikihow/__init__.py
@@ -4,6 +4,9 @@ from mycroft.messagebus.message import Message
 
 from os.path import abspath
 
+import requests
+import bs4
+
 __author__ = 'jarbas'
 
 logger = getLogger(abspath(__file__).split('/')[-2])
@@ -25,22 +28,10 @@ class WikiHowService(KnowledgeBackend):
             return
         else:
             dict = {}
-            how_to = {}
             # get knowledge about
             # TODO exceptions for erros
             try:
-                links = self.search_wikihow(subject)
-
-                if links == []:
-                    logger.error("No wikihow results")
-                    return
-
-                for link in links:
-                    title = self.get_title(link)
-                    steps = self.get_steps(link)
-                    how_to[title] = steps
-                    logger.info("how to " + title + " : " + str(steps))
-                # id info source
+                how_to = self.how_to(subject)
                 dict["wikihow"] = how_to
                 self.emit_node_info(dict)
             except:
@@ -48,21 +39,81 @@ class WikiHowService(KnowledgeBackend):
 
 
     def get_title(self, link):
-        # TODO get title of this how-to
-        logger.info("Getting title of " + link)
-        title = ""
+        # get title of this how-to
+        # print "Getting title of " + link
+        title = link.replace("http://www.wikihow.com/", "").replace("-", " ")
         return title
 
     def search_wikihow(self, search_term):
-        logger.info("Seaching wikihow for " + search_term)
-        # TODO get link and title of evey search result
+        # print "Seaching wikihow for " + search_term
+        search_url = "http://www.wikihow.com/wikiHowTo?search="
+        search_term_query = search_term.replace(" ", "+")
+        search_url += search_term_query
+        # print search_url
+        # open url
+        html = self.get_html(search_url)
+        soup = bs4.BeautifulSoup(html, "lxml")
+        # parse for links
         list = []
+        links = soup.findAll('a', attrs={'class': "result_link"})
+        for link in links:
+            url = "http:" + link.get('href')
+            list.append(url)
         return list
 
+    def get_html(self, url):
+        headers = {'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:41.0) Gecko/20100101 Firefox/41.0"}
+        r = requests.get(url, headers=headers)
+        html = r.text.encode("utf8")
+        return html
+
     def get_steps(self, url):
-        # TODO get step, and extended description
-        steps = {}
-        return steps
+        # open url
+        html = self.get_html(url)
+        soup = bs4.BeautifulSoup(html, "lxml")
+        # get steps
+        steps = []
+        ex_steps = []
+        step_html = soup.findAll("div", {"class": "step"})
+        for html in step_html:
+            trash = str(html.find("script"))
+            trash = trash.replace("<script>", "").replace("</script>", "").replace(";", "")
+            step = html.find("b")
+            step = step.text
+            ex_step = html.text.replace(trash, "")
+            steps.append(step)
+            ex_steps.append(ex_step)
+
+        # get step pic
+        pic_links = []
+        pic_html = soup.findAll("a", {"class": "image lightbox"})
+        for html in pic_html:
+            i = str(html).find("data-src=")
+            pic = str(html)[i:].replace('data-src="', "")
+            i = pic.find('"')
+            pic_links.append(pic[:i])
+
+        return steps, ex_steps, pic_links
+
+    def how_to(self, subject):
+        how_tos = {}
+        links = self.search_wikihow(subject)
+        if links == []:
+            print "No wikihow results"
+            return
+        for link in links:
+            how_to = {}
+            # how to title
+            title = self.get_title(link)
+            # get steps and pics
+            steps, descript, pics = self.get_steps(link)
+            how_to["title"] = title
+            how_to["steps"] = steps
+            how_to["detailed"] = descript
+            how_to["pics"] = pics
+            how_to["url"] = link
+            how_tos[title] = how_to
+        return how_tos
 
     def adquire(self, subject):
         logger.info('Call WikihowKnowledgeAdquire')
--- mycroft/knowledge/testing_ground.py
+++ mycroft/knowledge/testing_ground.py
@@ -1,39 +1,96 @@
-# initial idea to populate nodes
-
-import wptools
-
+import requests
+import bs4
 
 
-# wiki data
-# broken?
-musk = wptools.page('Stephen Fry').get_wikidata()
-
-print "description: " + musk.description
-print "what: " + musk.what
-print "data:"
-data = musk.wikidata
-for key in data:
-    print key + " : " + str(data[key])
-print "\nproperties:"
-props = musk.props
-for prop in props:
-    print prop + " : " + str(props[prop])
-
-subject = "Elon Musk"
-page = wptools.page(subject).get_query()
-node_data = {}
-node_data["pic"] = page.image('page')['url']
-node_data["name"] = page.label
-node_data["description"] = page.description
-node_data["summary"] = page.extext
-node_data["url"] = page.url
-
-for data in node_data:
-    print "\n" + data + " : " + str(node_data[data].encode('utf-8'))
+def get_title(link):
+    # get title of this how-to
+    # print "Getting title of " + link
+    title = link.replace("http://www.wikihow.com/", "").replace("-", " ")
+    return title
 
 
-print "\n infobox:"
-page = wptools.page(subject).get_parse()
-# TODO decent parsing, info is messy
-for entry in page.infobox:
-    print "\n" + entry + " : " + str(page.infobox[entry])
+def search_wikihow(search_term):
+    #print "Seaching wikihow for " + search_term
+    search_url = "http://www.wikihow.com/wikiHowTo?search="
+    search_term_query = search_term.replace(" ", "+")
+    search_url += search_term_query
+    # print search_url
+    # open url
+    html = get_html(search_url)
+    soup = bs4.BeautifulSoup(html, "lxml")
+    # parse for links
+    list = []
+    links = soup.findAll('a', attrs={'class': "result_link"})
+    for link in links:
+        url = "http:" + link.get('href')
+        list.append(url)
+    return list
+
+
+def get_html(url):
+    headers = {'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:41.0) Gecko/20100101 Firefox/41.0"}
+    r = requests.get(url, headers=headers)
+    html = r.text.encode("utf8")
+    return html
+
+
+def get_steps(url):
+    # open url
+    html = get_html(url)
+    soup = bs4.BeautifulSoup(html, "lxml")
+    # get steps
+    steps = []
+    ex_steps = []
+    step_html = soup.findAll("div", {"class": "step"})
+    for html in step_html:
+        trash = str(html.find("script"))
+        trash = trash.replace("<script>", "").replace("</script>", "").replace(";", "")
+        step = html.find("b")
+        step = step.text
+        ex_step = html.text.replace(trash, "")
+        steps.append(step)
+        ex_steps.append(ex_step)
+
+    # get step pic
+    pic_links = []
+    pic_html = soup.findAll("a", {"class": "image lightbox"})
+    for html in pic_html:
+        i = str(html).find("data-src=")
+        pic = str(html)[i:].replace('data-src="', "")
+        i = pic.find('"')
+        pic_links.append(pic[:i])
+
+    return steps, ex_steps, pic_links
+
+
+def how_to(subject):
+    how_tos = {}
+    links = search_wikihow(subject)
+    if links == []:
+        print "No wikihow results"
+        return
+    for link in links:
+        how_to = {}
+        # how to title
+        title = get_title(link)
+        # get steps and pics
+        steps, descript, pics = get_steps(link)
+        how_to["title"] = title
+        how_to["steps"] = steps
+        how_to["detailed"] = descript
+        how_to["pics"] = pics
+        how_to["url"] = link
+        how_tos[title] = how_to
+    return how_tos
+
+
+def main():
+    how_tos = how_to("boil an egg")
+    for how in how_tos:
+        print "How to " + how
+        i = 0
+        for step in how_tos[how]["steps"]:
+            print "step " + str(i) + " : " + step
+            print how_tos[how]["pics"][i]
+            i += 1
+        break

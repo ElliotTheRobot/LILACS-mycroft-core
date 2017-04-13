696eba1d48f4eaecec73403b4a4e38208a961e63
jarbasai <jarbasai>
2017-04-13 22:04:08 +0100

    scrap resource page

--- mycroft/knowledge/services/dbpedia/__init__.py
+++ mycroft/knowledge/services/dbpedia/__init__.py
@@ -5,6 +5,8 @@ from mycroft.messagebus.message import Message
 from os.path import abspath
 
 import spotlight
+import json
+import urlfetch
 
 host = "http://spotlight.sztaki.hu:2222/rest/annotate"
 
@@ -52,7 +54,7 @@ class DBpediaService(KnowledgeBackend):
                     # dbpedia link
                     url = annotation["URI"]
                     node["url"] = url
-
+                    node["page_info"] = self.scrap_resource_page(url)
                     node_data[subject] = node
 
                 # id info source
@@ -61,6 +63,62 @@ class DBpediaService(KnowledgeBackend):
             except:
                 logger.error("Could not parse dbpedia for " + str(subject))
 
+    def scrap_resource_page(self, link):
+        u = link.replace("http://dbpedia.org/resource/", "http://dbpedia.org/data/") + ".json"
+        data = urlfetch.fetch(url=u)
+        json_data = json.loads(data.content)
+        dbpedia = {}
+        for j in json_data[link]:
+            if "#seeAlso" in j:
+                # complimentary nodes
+                dbpedia["see_also"] = []
+                for entry in json_data[link][j]:
+                    value = entry["value"]
+                    dbpedia["see_also"].append(value)
+            elif "wikiPageExternalLink" in j:
+                # links about this subject
+                dbpedia["external_links"] = []
+                for entry in json_data[link][j]:
+                    value = entry["value"]
+                    dbpedia["external_links"].append(value)
+            elif "subject" in j:
+                # relevant nodes
+                dbpedia["related_subjects"] = []
+                for entry in json_data[link][j]:
+                    value = entry["value"]
+                    dbpedia["related_subjects"].append(value)
+            elif "abstract" in j:
+                # english description
+                dbpedia["abstract"] = \
+                [abstract['value'] for abstract in json_data[link][j] if abstract['lang'] == 'en'][0]
+            elif "depiction" in j:
+                # pictures
+                dbpedia["picture"] = []
+                for entry in json_data[link][j]:
+                    value = entry["value"]
+                    dbpedia["picture"].append(value)
+            elif "isPrimaryTopicOf" in j:
+                # usually original wikipedia link
+                dbpedia["primary"] = []
+                for entry in json_data[link][j]:
+                    value = entry["value"]
+                    #dbpedia["primary"].append(value)
+            elif "wasDerivedFrom" in j:
+                # usually wikipedia link at scrap date
+                dbpedia["derived_from"] = []
+                for entry in json_data[link][j]:
+                    value = entry["value"]
+                    #dbpedia["derived_from"].append(value)
+            elif "owl#sameAs" in j:
+                # links to dbpedia in other languages
+                dbpedia["same_as"] = []
+                for entry in json_data[link][j]:
+                    value = entry["value"]
+                    if "resource" in value:
+                        #dbpedia["same_as"].append(value)
+                        pass
+
+        return dbpedia
 
     def adquire(self, subject):
         logger.info('Call DBpediaKnowledgeAdquire')
@@ -70,7 +128,8 @@ class DBpediaService(KnowledgeBackend):
         # TODO emit node_info for node manager to update/create node
         for source in info:
             for key in info[source]:
-                print "\n" + key + " : " + str(info[source][key])
+                pass
+                #print "\n" + key + " : " + str(info[source][key])
         self.emitter.emit(Message('DBPediaResult', {"dbpedia": info}))
 
     def stop(self):

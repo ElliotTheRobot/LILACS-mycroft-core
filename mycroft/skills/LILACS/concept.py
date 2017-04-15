7e7c0a4cd209075cf99e5e7ffd31960fe3e59669
jarbasai <jarbasai>
2017-04-15 19:44:35 +0100

    removed cstorage references

--- mycroft/skills/LILACS/concept.py
+++ mycroft/skills/LILACS/concept.py
@@ -244,8 +244,6 @@ class ConceptNode():
 
 class ConceptConnector():
 
-    storageLoaded = False
-
     def __init__(self, concepts = {}):
         self.concepts = concepts
         self.logger = getLogger("ConceptConnector")
